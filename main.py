d152726d71d2539d5c3f47e8f8b74b876d0126b9
piology <pius.grossmann@web.de>
2022-11-30 15:13:24 +0100

    started with RFID_device 2

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import usb.core
+import usb.util
 
+#   idVendor           0x072f Advanced Card Systems, Ltd
+#   idProduct          0x2200 ACR122U
 
 RFID_device = usb.core.find(idVendor=0x072f, idProduct=0x2200)
-endpoint = RFID_device[0].interfaces()[0].endpoints()[0]
-i = RFID_device[0].interfaces()[0].bInterfaceNumber
 
-RFID_device.reset()
 
-if RFID_device.is_kernel_driver_active():
-    RFID_device.detach_kernel_driver()
+# RFID_device = usb.core.find(idVendor=0x072f, idProduct=0x2200)
+# endpoint = RFID_device[0].interfaces()[0].endpoints()[0]
+# interface = RFID_device[0].interfaces()[0].bInterfaceNumber
 
-RFID_device.set_configuration()
+# RFID_device.reset()
 
-eaddr = endpoint.bEndpointAddress
+# if RFID_device.is_kernel_driver_active(interface):
+#     RFID_device.detach_kernel_driver(interface)
 
-r = RFID_device.read(eaddr, 100)
-# print(RFID_device[0].interfaces())
+# RFID_device.set_configuration()
+
+# eaddr = endpoint.bEndpointAddress
+
+# r = RFID_device.read(eaddr, 100)
+# # print(RFID_device[0].interfaces())
